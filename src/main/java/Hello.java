a7e2fc200da1697f484edc2a027645b0b811762b
unknown <liurui@liurui.microstarsoft.com>
2014-07-11 15:09:42 +0800

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+/**
+ * Created by liurui on 2014/7/10.
+ */
+public class Hello {
+    static Log logger = LogFactory.getLog(Hello.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
